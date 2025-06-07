00c3090a2ae7d41e4b08e4dba3c37f2d0f430f7e
Emmanuel <oketunbi.olufunke@gmail.com>
2025-06-07 08:38:37 -0700

    paystack payment api integrated

--- app/libs/component.js
+++ app/libs/component.js
@@ -1,14 +1,15 @@
 "use client"
 
 import { auth } from "@/firebase/firebase-client";
-import logOut from "../actions/main";
-import {  useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import logOut, { updateBalance } from "../actions/main";
 import Image from "next/image";
 import Logo from "../images/logo.png"
 import cancelIcon from "../images/cancel.png"
 import Link from "next/link";
 import "../styles/main.css"
+import { useEffect, useRef, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { usePaystackPayment } from "react-paystack";
 
  
 
@@ -28,6 +29,7 @@ export function SideBar() {
             <Image src={Logo} width={30} height={30} alt="Logo"/>
             <h2 className="font-sans font-bold">VectorPay</h2>
         </section>
+
         <ul>
             <li>
             <img src={"https://img.icons8.com/?size=100&id=87013&format=png&color=000000"} alt="dashboard-icon"/>
@@ -213,15 +215,127 @@ export function Statistics() {
 
 
 export function DepositModal() {
+
+    const modalRef = useRef(null)
+
+   const closeModal = ()=>{
+    if (modalRef.current) {
+        modalRef.current.close()
+    }
+   }
+
+   function handleModalToggle() {
+        closeModal()
+        document.getElementById("my_modal_4").showModal()
+   }
+
     return (
-        <dialog id="my_modal_3" className="modal">
-            <div className="modal-box">
+        <dialog id="my_modal_3" className="modal modal-bottom md:modal-middle" ref={modalRef}>
+            <div className="modal-box p-10">
                 <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <button className="btn btn-md btn-circle btn-ghost absolute right-2 top-1">✕</button>
                 </form>
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                <div className="modal-content-container">
+                   <article>
+                    <h2 >Add Funds</h2>
+                    <p >How do you want to topup?</p>
+                   </article>
+
+                   <section className="modal-content ">
+                        <div>
+                            <h2>Request Money</h2>
+                            <p>Request money from beneficiaries.</p>
+                        </div>
+                        <div onClick={handleModalToggle}>
+                            <h2>ATM Card</h2>
+                            <p>Fund your wallet using your ATM Card.</p>
+                        </div>
+                   </section>
+                </div>
+            </div>
+        </dialog>
+    )
+}
+
+export function DepositAmountModal() {
+    const modalRef = useRef(null)
+    const [amount, setAmount] = useState(null)
+    const [currentUser, setcurrentUser] = useState({name: "", email: "", isVerified: null})
+
+
+    useEffect(()=>{
+        onAuthStateChanged(auth,user=>setcurrentUser({name:user?.displayName, email: user?.email, isVerified:user?.emailVerified }))
+    },[])
+
+    const closeModal = ()=>{
+        if (modalRef.current) {
+            modalRef.current.close()
+        }
+       }
+    
+       function handleModalToggle() {
+            closeModal()
+            document.getElementById("my_modal_3").showModal()
+       }
+
+    const config = {
+        reference: new Date().getTime().toString(),
+        email:currentUser?.email,
+        amount: amount * 100,
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        metadata: {
+          name: currentUser?.name,
+        },
+    }
+
+
+    const initializePayment = usePaystackPayment(config)
+
+   async function onSuccess() {
+       await updateBalance(amount)
+    }
+
+
+    function handleDeposit() {
+        try {
+            initializePayment({onSuccess})
+            closeModal()
+        } catch (error) {
+            console.log(error?.message);
+            
+        }
+    }
+
+    return (
+        <dialog id="my_modal_4" className="modal modal-bottom md:modal-middle" ref={modalRef}>
+            <div className="modal-box p-10">
+                <button onClick={handleModalToggle} className="modal-back-btn"> <img src="https://img.icons8.com/?size=100&id=79026&format=png&color=000000" alt="back-icon" className="w-3 mr-1 h-3"/> <span>Back</span></button>
+                <form method="dialog">
+                    <button className="btn btn-md btn-circle btn-ghost absolute right-2 top-1">✕</button>
+                </form>
+                <div className="modal-amount-container">
+                    <article>
+                        <h2 >ATM Card</h2>
+                        <p >Add funds with ATM Card</p>
+                    </article>
+                    
+                   <section>
+                        <label htmlFor="amount">Amount:</label>
+                            
+                        <input
+                        
+                            type="number"
+                            name="amount"
+                            className="input validator"
+                            value={amount}
+                            onChange={(e)=>setAmount(e.target.value)}
+                            required
+                            placeholder="Enter Amount"
+                            title="Must be between be a number"
+                            />
+                            <button onClick={handleDeposit} disabled={amount <= 0}>Continue</button>
+                   </section>
+                </div>
             </div>
         </dialog>
     )
