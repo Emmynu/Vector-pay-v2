284016e2e5f39982692270d6a44cb77d66e8344d
Emmanuel <oketunbi.olufunke@gmail.com>
2026-07-06 00:43:25 -0700

    disconnecting from railway

--- client/app/dashboard/page.jsx
+++ client/app/dashboard/page.jsx
@@ -3,7 +3,6 @@ function Dashboard() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
             <p className="text-lg">Welcome to the dashboard!</p>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores tempore enim culpa assumenda, cupiditate sapiente exercitationem ratione voluptatum repudiandae labore velit, nostrum perspiciatis perferendis distinctio! Recusandae ea nam aperiam deleniti.</h3>
         </div>
      );
 }
