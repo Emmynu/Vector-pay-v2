5f7ebcf255afc15194ddffefa0e75fb50cd03b01
Emmanuel <oketunbi.olufunke@gmail.com>
2026-08-29 10:19:14 -0700

    next-config updated

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return[
             {
                 source: "/api/v1/:path*",
-                destination:"https://next-tuts-jet.vercel.app/api/v1/:path*"
+                destination:"https://vector-pay.onrender.com/api/v1/:path*"
             }
         ]
     }
